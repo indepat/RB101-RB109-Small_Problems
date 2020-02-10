e3f1ac4d6628a729ed9d171fb0ad6e2aa291c274
Patrick Indekwe <indepat1@gmail.com>
2020-02-10 11:59:49 -0500

    adding other exercices

--- Easy_4/convert_hexa_to_string.rb
+++ Easy_4/convert_hexa_to_string.rb
@@ -1,22 +1,7 @@
-#Write a method that takes a String of digits, and returns the appropriate number as an integer. 
+#Write a method that takes a hexadecimal, and returns the appropriate number as an integer. 
 #You may not use any of the methods mentioned above.
 #You may not use any of the standard conversion methods available in Ruby, such as String#to_i, Integer(), etc. 
 #Your method should do this the old-fashioned way and calculate the result by analyzing the characters in the string.
-
-HEXADECIMAL = {
-  '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
-  '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, 
-  'A' => 10, 'B' => 11, 'C' => 12, 'D' => 13, 'E' => 14, 'F' => 15
-}
-
-def hexadecimal_to_integer(hexadecimal)
-  hexadecimal_array = hexadecimal.upcase!.chars.map { |char| HEXADECIMAL[char]  }
-  value = 0
-  hexadecimal_array.each_with_index { |hexa,index| value = (16 ** index) + hexa}
-  
-end
-
-hexadecimal_to_integer('4D9f')
 
 
 
---
+++ Easy_4/convert_string_to_number.rb
@@ -0,0 +1,19 @@
+#Write a method that takes a String of digits, and returns the appropriate number as an integer. 
+#You may not use any of the methods mentioned above.
+#You may not use any of the standard conversion methods available in Ruby, such as String#to_i, Integer(), etc. 
+#Your method should do this the old-fashioned way and calculate the result by analyzing the characters in the string.
+
+STRING_CONVERT_NUMERICAL = {
+  '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
+  '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, 
+}
+
+def string_to_integer(string)
+  digits = string.chars.map { |char| STRING_CONVERT_NUMERICAL[char]  }
+  value = 0
+  digits.each { |digit| value = 10* value + digit}
+  
+end
+
+
+
